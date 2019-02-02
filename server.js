845c6cd627b4ad10bbb18b6841ab49e30d859c67
kan tippayamontri <kantippayamontri@gmail.com>
2019-02-02 16:06:19 +0700

    code gift

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = express()
 
 const config = {
     channelAccessToken: 'WEMkqKuhpz/n4cFDUCE58nbCAxN8/yDf1xTimYw9qlVTc7Y2xpAbmS9p2Y6j3CwQ27F+R+6lVX3JlU5n5C+V8+H8EcEbd3MtXvHi0ZqiPj95jjjr4OtY/YP/xWaMOARWUWZ64gaBbBe+7rv0Iv5kKwdB04t89/1O/w1cDnyilFU=',
-    channelSecret: '99854f6e3ac0a9d90b5313247821f18a'
-};
+  channelSecret: '99854f6e3ac0a9d90b5313247821f18a'
+  }
 
 const client = new line.Client(config);
 
