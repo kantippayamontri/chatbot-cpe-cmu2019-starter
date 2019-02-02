208d302078d5f1167fef8302c806b01a5bd2c8a1
kan tippayamontri <kantippayamontri@gmail.com>
2019-02-02 14:40:05 +0700

    template
    dsfadsfadf

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.post('/webhook', middleware(config), (req, res) => {
   const message = event.message;  
       if (event.type === 'message') {  
         console.log(message)
-        if(message.type == "text"){
+      /*  if(message.type == "text"){
         client.replyMessage(event.replyToken, {
           type: 'text',
           text: message.text,
@@ -36,7 +36,44 @@ app.post('/webhook', middleware(config), (req, res) => {
           stickerId:message.stickerId
         });
 
-      }
+      }*/
+
+      client.replyMessage(event.replyToken, 
+        {
+          "type": "template",
+          "altText": "This is a buttons template",
+          "template": {
+              "type": "buttons",
+              "thumbnailImageUrl": "https://vignette.wikia.nocookie.net/line/images/b/bb/2015-brown.png/revision/latest?cb=20150808131630",
+              "imageAspectRatio": "rectangle",
+              "imageSize": "cover",
+              "imageBackgroundColor": "#FFFFFF",
+              "title": "Menu",
+              "text": "Please select",
+              "defaultAction": {
+                  "type": "uri",
+                  "label": "View detail",
+                  "uri": "http://google.com/"
+              },
+              "actions": [
+                  {
+                    "type": "postback",
+                    "label": "Buy",
+                    "data": "action=buy&itemid=123"
+                  },
+                  {
+                    "type": "message",
+                    "label": "Add to cart",
+                    "text": "no no no"
+                  },
+                  {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://google.com"
+                  }
+              ]
+          }
+        })
 
       }
 
